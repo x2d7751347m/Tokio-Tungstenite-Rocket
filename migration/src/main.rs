a172b4e35e8bf19f6232f6a6360532bed7a71cd9
x2d7751347m <x2d7751347m@gmail.com>
2023-07-18 01:16:32 +0900

    add secret key

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,3 +1,4 @@
+use config::app_config::AppConfig;
 use sea_orm_migration::prelude::*;
 
 #[async_std::main]
@@ -6,11 +7,12 @@ async fn main() {
     //  Setting `DATABASE_URL` environment variable
     let key = "DATABASE_URL";
     if std::env::var(key).is_err() {
-        // Getting the database URL from Rocket.toml if it's not set
-        let figment = rocket::Config::figment();
-        let database_url: String = figment
-            .extract_inner("databases.sea_orm.url")
-            .expect("Cannot find Database URL in Rocket.toml");
+        // // Getting the database URL from Rocket.toml if it's not set
+        // let figment = rocket::Config::figment();
+        // let database_url: String = figment
+        //     .extract_inner("databases.sea_orm.url")
+        //     .expect("Cannot find Database URL in Rocket.toml");
+        let database_url: String = AppConfig::default().db_url;
         std::env::set_var(key, database_url);
     }
 
